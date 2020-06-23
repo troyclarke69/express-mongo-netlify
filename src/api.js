f3eaacc598f11aed29c8430bf1d7a67b70e01ad8
Troy Clarke <troy.clarke87@gmail.com>
2020-06-23 17:41:31 -0400

    updates

--- src/api.js
+++ src/api.js
@@ -1,20 +1,60 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mongoose = require('mongoose');
+
+const prodctSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Number, default: 0, required: true },
+  category: { type: String, required: true },
+  countInStock: { type: Number, default: 0, required: true },
+  description: { type: String, required: true },
+  rating: { type: Number, default: 0, required: true },
+  numReviews: { type: Number, default: 0, required: true },
+});
+const Product = mongoose.model("Product", prodctSchema);
+
+// const mongodbUrl = config.MONGODB_URL;
+const mongodbUrl = 'mongodb+srv://troy:Gruj3267@cluster0-orxh2.gcp.mongodb.net/goshop';
+
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).catch((error) => 
+  console.log(error.reason));
 
 const app = express();
-const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+const router = express.Router();
+// router.get("/", (req, res) => {
+//   res.json({
+//     hello: "hi!"
+//   });
+// });
+
+router.get("/", async (req, res) => {
+  const category = req.query.category ? { category: req.query.category } : {};
+  const searchKeyword = req.query.searchKeyword ? {
+    name: {
+      $regex: req.query.searchKeyword,
+      $options: 'i'
+    }
+  } : {};
+  const sortOrder = req.query.sortOrder ?
+    (req.query.sortOrder === 'lowest' ? { price: 1 } : { price: -1 })
+    :
+    { _id: -1 };
+  const products = await Product.find({ ...category, ...searchKeyword }).sort(sortOrder);
+  res.send(products);
 });
 
 app.use(`/.netlify/functions/api`, router);
-
 module.exports = app;
 module.exports.handler = serverless(app);
-
 
 //npm install express netlify-lambda serverless-http
 //npm run build
